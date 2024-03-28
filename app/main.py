974af9a6adb105544707b084abab6ec17325d212
Brian-Bowyer <bobsnake64@gmail.com>
2024-03-28 00:12:36 -0500

    better classes and abstractify a bit

--- app/main.py
+++ app/main.py
@@ -3,24 +3,38 @@ from nicegui import ui
 
 @ui.page("/")
 def main():
-    with ui.row().classes("full-width full-height border flex-wrap"):
-        with ui.column().classes("flex-auto border"):
-            with ui.card():
-                ui.label("1")
+    with ui.row().classes("w-full h-full border flex-wrap"):
+        columns = [ui.column().classes("flex-auto border") for _ in range(3)]
+        with columns[0]:
+            cards = [ui.card().classes("w-full items-center") for _ in range(3)]
+            with cards[0]:
+                ui.label("Lorem ipsum")
 
-            with ui.card():
-                ui.label("2")
+            with cards[1]:
+                ui.label("Lorem ipsum")
 
-            with ui.card():
-                ui.label("3")
-        with ui.column().classes("flex-auto border"):
-            ui.label("4")
-            ui.label("5")
-            ui.label("6")
-        with ui.column().classes("flex-auto border"):
-            ui.label("7")
-            ui.label("8")
-            ui.label("9")
+            with cards[2]:
+                ui.label("Lorem ipsum")
+        with columns[1]:
+            cards = [ui.card().classes("w-full items-center") for _ in range(3)]
+            with cards[0]:
+                ui.label("Lorem ipsum")
+
+            with cards[1]:
+                ui.label("Lorem ipsum")
+
+            with cards[2]:
+                ui.label("Lorem ipsum")
+        with columns[2]:
+            cards = [ui.card().classes("w-full items-center") for _ in range(3)]
+            with cards[0]:
+                ui.label("Lorem ipsum")
+
+            with cards[1]:
+                ui.label("Lorem ipsum")
+
+            with cards[2]:
+                ui.label("Lorem ipsum")
 
 
 ui.run()
