1041ba0e8373e904709ddfae26aaeb3551241589
Brian-Bowyer <bobsnake64@gmail.com>
2024-03-27 22:52:30 -0500

    skeleton

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
+from nicegui import ui
+
+
+@ui.page("/")
 def main():
-    pass
+    with ui.row().classes("full-width full-height border flex-wrap"):
+        with ui.column().classes("flex-auto border"):
+            with ui.card():
+                ui.label("1")
+
+            with ui.card():
+                ui.label("2")
+
+            with ui.card():
+                ui.label("3")
+        with ui.column().classes("flex-auto border"):
+            ui.label("4")
+            ui.label("5")
+            ui.label("6")
+        with ui.column().classes("flex-auto border"):
+            ui.label("7")
+            ui.label("8")
+            ui.label("9")
 
 
-if __name__ == "__main__":
-    main()
+ui.run()
