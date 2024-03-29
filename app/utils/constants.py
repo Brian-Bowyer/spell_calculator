3143e1df65ddd34519ccc38235f938020ae1ee9e
Brian-Bowyer <bobsnake64@gmail.com>
2024-03-29 12:27:53 -0500

    add some constants

---
+++ app/utils/constants.py
@@ -0,0 +1,16 @@
+import os
+
+ARCANA = [
+    "Death",
+    "Fate",
+    "Forces",
+    "Life",
+    "Matter",
+    "Mind",
+    "Prime",
+    "Space",
+    "Spirit",
+    "Time",
+]
+
+STORAGE_SECRET = os.getenv("STORAGE_SECRET", "local_storage_secret")
